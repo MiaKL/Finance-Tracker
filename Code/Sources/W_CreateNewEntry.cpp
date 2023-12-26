a11f7c78fa85ae25aca1107468a2b1aa37dbe882
Mia Kelley-Lanser <89313852+MiaKL@users.noreply.github.com>
2023-12-26 18:35:16 -0500

    Add files via upload

---
+++ Code/Sources/W_CreateNewEntry.cpp
@@ -0,0 +1,38 @@
+#include "W_CreateNewEntry.h"
+#include "ui_W_CreateNewEntry.h"
+
+#include "UserDatabase.h"
+
+#include <QMessageBox>
+
+W_CreateNewEntry::W_CreateNewEntry(UserDatabase* database, QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::W_CreateNewEntry)
+{
+    ui->setupUi(this);
+    this->database = database;
+}
+
+W_CreateNewEntry::~W_CreateNewEntry()
+{
+    delete ui;
+}
+
+void W_CreateNewEntry::on_pushButton_Create_clicked()
+{
+    QString type = ui->comboBox_Type->currentText();
+    QString category = ui->lineEdit_Category->text();
+    QString subcategory = ui->lineEdit_Subcategory->text();
+    double amount = ui->doubleSpinBox_Amount->value();
+    QDate date = ui->dateEdit->date();
+
+    if (type == "Expense") amount *= -1.0;
+
+    if (category == "") QMessageBox::information(this, "Invalid Category", "Category field is required.");
+    else {
+        if (subcategory.toStdString() == "") database->createNewEntry(date.month(), date.day(), date.year(), category.toStdString(), amount);
+        else database->createNewEntry(date.month(), date.day(), date.year(), category.toStdString(), subcategory.toStdString(), amount);
+        this->hide();
+    }
+}
+
