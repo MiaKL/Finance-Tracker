101298bdc6ef122911529f09d9349d2dd54f6729
Mia Kelley-Lanser <89313852+MiaKL@users.noreply.github.com>
2023-12-26 18:30:13 -0500

    Add files via upload

---
+++ Code/Headers/W_GenerateGraph.h
@@ -0,0 +1,54 @@
+/*
+ * Author: Mia Kelley-Lanser
+ * Class: Advanced C++
+ * Date: 05/07/2023
+*/
+
+#ifndef W_GENERATEGRAPH_H
+#define W_GENERATEGRAPH_H
+
+#include <QDialog>
+
+#include "UserDatabase.h"
+
+namespace Ui {
+class W_GenerateGraph;
+}
+
+class W_GenerateGraph : public QDialog
+{
+    Q_OBJECT
+
+public:
+    /**
+     * @brief W_DisplayEntries constructor; sets database and parent to the given values;
+     * calls makePlot method wtih no argument (defaults to "") so that a graph is made
+     * and shown of all income and expenses over time
+     *
+     * @param database
+     * @param parent
+     */
+    explicit W_GenerateGraph(UserDatabase* database, QWidget *parent = nullptr);
+    ~W_GenerateGraph();
+
+private slots:
+    /**
+     * @brief When the Display button is clicked, clears the graph and calls makePlot
+     * method with the category in the ui line-edit
+     */
+    void on_pushButton_Display_clicked();
+
+private:
+    Ui::W_GenerateGraph *ui;
+    UserDatabase* database;
+
+    /**
+     * @brief Makes a graph of the income and expenses over time in the given category
+     * (if no category is given, displays data in all categories)
+     *
+     * @param category
+     */
+    void makePlot(QString category = "");
+};
+
+#endif // W_GENERATEGRAPH_H
