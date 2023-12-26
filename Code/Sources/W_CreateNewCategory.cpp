7916e68ccef9de6c27978662d95d3bcfe8b2bf0b
Mia Kelley-Lanser <89313852+MiaKL@users.noreply.github.com>
2023-12-26 18:34:56 -0500

    Add files via upload

---
+++ Code/Sources/W_CreateNewCategory.cpp
@@ -0,0 +1,37 @@
+#include "W_CreateNewCategory.h"
+#include "ui_W_CreateNewCategory.h"
+
+#include <QMessageBox>
+
+W_CreateNewCategory::W_CreateNewCategory(UserDatabase* database, QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::W_CreateNewCategory)
+{
+    ui->setupUi(this);
+    this->database = database;
+}
+
+W_CreateNewCategory::~W_CreateNewCategory()
+{
+    delete ui;
+}
+
+void W_CreateNewCategory::on_pushButton_Create_clicked()
+{
+    QString title = ui->lineEdit_Title->text();
+    QString subcategory = ui->lineEdit_Subcategory->text();
+
+    if (title == "") QMessageBox::information(this, "Invalid Title", "Title field is required.");
+    else {
+        bool created = true;
+        try {
+            if (subcategory.toStdString() == "") database->createNewCategory(title.toStdString());
+            else database->createNewSubcategory(title.toStdString(), subcategory.toStdString());
+        } catch (std::invalid_argument e) {
+            created = false;
+            QMessageBox::information(this, "Invalid category or subcategory.", e.what());
+        }
+        if (created) this->hide();
+    }
+}
+
